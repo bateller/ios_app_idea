ed6e5f581a7abb607863259305d91d9fdd241dcf
Brian A. Teller <brianateller@Brians-MBP.wbat.net>
2016-08-09 21:33:39 -0400

    first commit

---
+++ www/js/constants.js
@@ -0,0 +1,10 @@
+angular.module('starter')
+ 
+.constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated'
+})
+ 
+.constant('API_ENDPOINT', {
+  url: 'https://www.wbat.net/rest_api/v1'
+  //  For a simulator use: url: 'http://127.0.0.1:8080/api'
+});
